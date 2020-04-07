5cdfed40ecde9243bbd2287d980d5c41adab61bb
Quan <urjhams@gmail.com>
2020-04-07 22:14:45 +0200

    init

---
+++ Pictionarrry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pictionarrry
+//
+//  Created by Quân Đinh on 07.04.20.
+//  Copyright © 2020 Quân Đinh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
